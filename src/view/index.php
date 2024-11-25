36d3d291c59112e388fb53ca70c31db3cf54d98b
dhhhdh <hothanhan51@gmail.com>
2024-11-26 06:56:36 +0700

    Index full

--- src/view/index.php
+++ src/view/index.php
@@ -806,84 +806,85 @@
                                 </li>
     
                             </ul>
-    
-                            <ul class="footer-list">
-    
-                                <li>
-                                    <p class="footer-list-title">Contact Info</p>
-                                </li>
-    
-                                <li>
-                                    <p class="footer-list-item">+1 (062) 109-9222</p>
-                                </li>
-    
-                                <li>
-                                    <p class="footer-list-item">baolhn.23ns@vku.udn.vn</p>
-                                </li>
-    
-                                <li>
-                                    <address class="footer-list-item">
-                                        Số 88, Đường Hải Vân Thượng, Phường Hòa Minh, Quận Liên Chiểu, Đà Nẵng.
-                                    </address>
-                                </li>
-                            </ul>
-    
-                            <ul class="footer-list">
-    
-                                <li>
-                                    <p class="footer-list-title">Opening Hours</p>
-                                </li>
-    
-                                <li>
-                                    <p class="footer-list-item">Monday-Friday: 08:00-22:00</p>
-                                </li>
-    
-                                <li>
-                                    <p class="footer-list-item">Tuesday 4PM: Till Mid Night</p>
-                                </li>
-    
-                                <li>
-                                    <p class="footer-list-item">Saturday: 10:00-16:00</p>
-                                </li>
-                            </ul>
-    
-                            <form action="" class="footer-form">
-    
-                                <p class="footer-list-title">Book a Table</p>
-    
-                                <div class="input-wrapper">
-    
-                                    <input type="text" name="full_name" 
-                                    required placeholder="Your Name" aria-label="Your Name"
-                                     class="input-field">
-    
-                                     <input type="email" name="email address" 
-                                     required placeholder="Email" aria-label="Email"
-                                      class="input-field">
-    
-                                </div>
-    
-                                <div class="input-wrapper">
-                                    <select name="total_person" aria-label="Total person" class="input-field">
-                                        <option value="person">Person</option>
-                                        <option value="2 person">2 Person</option>
-                                        <option value="3 person">3 Person</option>
-                                        <option value="4 person">4 Person</option>
-                                        <option value="5 person">5 Person</option>
-                                    </select>
-    
-                                    <input type="date" name="booking_date" aria-label="Reservation date"
-                                     required class="input-field">
-    
-                                     <textarea name="message" aria-label="Message" placeholder="Message" class="input-field"></textarea>
-    
-                                     <button type="submit" class="btn btn-fill">Book a Table</button>
-    
-                                </div>
-                            </form>
-    
-    
+
                         </div>
+
+                        <ul class="footer-list">
+    
+                            <li>
+                                <p class="footer-list-title">Contact Info</p>
+                            </li>
+
+                            <li>
+                                <p class="footer-list-item">+1 (062) 109-9222</p>
+                            </li>
+
+                            <li>
+                                <p class="footer-list-item">baolhn.23ns@vku.udn.vn</p>
+                            </li>
+
+                            <li>
+                                <address class="footer-list-item">
+                                    Số 88, Đường Hải Vân Thượng, Phường Hòa Minh, Quận Liên Chiểu, Đà Nẵng.
+                                </address>
+                            </li>
+                        </ul>
+
+                        <ul class="footer-list">
+
+                            <li>
+                                <p class="footer-list-title">Opening Hours</p>
+                            </li>
+
+                            <li>
+                                <p class="footer-list-item">Monday-Friday: 08:00-22:00</p>
+                            </li>
+
+                            <li>
+                                <p class="footer-list-item">Tuesday 4PM: Till Mid Night</p>
+                            </li>
+
+                            <li>
+                                <p class="footer-list-item">Saturday: 10:00-16:00</p>
+                            </li>
+                        </ul>
+
+                        <form action="" class="footer-form">
+
+                            <p class="footer-list-title">Book a Table</p>
+
+                            <div class="input-wrapper">
+
+                                <input type="text" name="full_name" 
+                                required placeholder="Your Name" aria-label="Your Name"
+                                 class="input-field">
+
+                                 <input type="email" name="email address" 
+                                 required placeholder="Email" aria-label="Email"
+                                  class="input-field">
+
+                            </div>
+
+                            <div class="input-wrapper">
+                                <select name="total_person" aria-label="Total person" class="input-field">
+                                    <option value="person">Person</option>
+                                    <option value="2 person">2 Person</option>
+                                    <option value="3 person">3 Person</option>
+                                    <option value="4 person">4 Person</option>
+                                    <option value="5 person">5 Person</option>
+                                </select>
+
+                                <input type="date" name="booking_date" aria-label="Reservation date"
+                                 required class="input-field">
+
+                            </div>
+
+                            <textarea name="message" aria-label="Message" placeholder="Message" class="input-field"></textarea>
+
+                            <button type="submit" class="btn btn-fill">Book a Table</button>
+
+                        </form>
+
                     </div>
                 </div>
 
